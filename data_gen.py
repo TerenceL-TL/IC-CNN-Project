5e8bc85153716224791d1ba22ffdd20880418195
Terence-TL <mirac318@outlook.com>
2024-07-12 00:42:45 +0100

    change png to npy in case data depreciate

--- data_gen.py
+++ data_gen.py
@@ -1,79 +1,79 @@
 import os
 import numpy as np
-import nibabel as nib
-import matplotlib.pyplot as plt
 import pandas as pd
 import glob
 import random
+import matplotlib.pyplot as plt
 
-import cv2
-
-from skimage.color import rgb2gray
-from skimage.morphology import label
 from skimage.transform import resize
 from sklearn.model_selection import train_test_split
-from skimage.io import imread, imshow, concatenate_images
-
 from keras.preprocessing.image import ImageDataGenerator
 
 def create_df(data_dir):
-    images_paths = glob.glob(f'{data_dir}image/*.png')
-    masks_paths = glob.glob(f'{data_dir}masks/*.png*')
+    images_paths = glob.glob(f'{data_dir}image/*.npy')
+    masks_paths = glob.glob(f'{data_dir}masks/*.npy')
 
     df = pd.DataFrame(data= {'images_paths': images_paths, 'masks_paths': masks_paths})
 
     return df
 
-# Function to split dataframe into train, valid, test
 def split_df(df):
-    # create train_df
-    train_df, dummy_df = train_test_split(df, train_size= 0.8)
-
-    # create valid_df and test_df
-    valid_df, test_df = train_test_split(dummy_df, train_size= 0.5)
-
+    train_df, dummy_df = train_test_split(df, train_size=0.8)
+    valid_df, test_df = train_test_split(dummy_df, train_size=0.5)
     return train_df, valid_df, test_df
 
 def create_gens(df, aug_dict):
     img_size = (240, 240)
     batch_size = 16
 
-
     img_gen = ImageDataGenerator(**aug_dict)
     msk_gen = ImageDataGenerator(**aug_dict)
 
-    # Create general generator
-    image_gen = img_gen.flow_from_dataframe(df, x_col='images_paths', class_mode=None, color_mode='grayscale', target_size=img_size,
-                                            batch_size=batch_size, save_to_dir=None, save_prefix='image', seed=1)
+    while True:
+        for i in range(0, len(df), batch_size):
+            batch_df = df.iloc[i:i+batch_size]
 
-    mask_gen = msk_gen.flow_from_dataframe(df, x_col='masks_paths', class_mode=None, color_mode='grayscale', target_size=img_size,
-                                            batch_size=batch_size, save_to_dir=None, save_prefix= 'mask', seed=1)
+            images = []
+            masks = []
 
-    gen = zip(image_gen, mask_gen)
+            for _, row in batch_df.iterrows():
+                img = np.load(row['images_paths'])
+                msk = np.load(row['masks_paths'])
 
-    for (img, msk) in gen:
-        img = img / 255
-        msk = msk / 255
-        msk[msk > 0.5] = 1
-        msk[msk <= 0.5] = 0
+                # Resize if needed
+                img = resize(img, img_size, anti_aliasing=True)
+                msk = resize(msk, img_size, anti_aliasing=True)
 
-        yield (img, msk)
+                # Add channel dimension
+                img = np.expand_dims(img, axis=-1)
+                msk = np.expand_dims(msk, axis=-1)
+
+                images.append(img)
+                masks.append(msk)
+
+            images = np.array(images)
+            masks = np.array(masks)
+
+            # Normalize images and masks
+            images = images / 255.0
+            masks = masks / 255.0
+            masks[masks > 0.5] = 1
+            masks[masks <= 0.5] = 0
+
+            yield (images, masks)
 
 def show_images(images, masks):
     plt.figure(figsize=(12, 12))
     for i in range(25):
-        plt.subplot(5, 5, i+1)
+        plt.subplot(5, 5, i + 1)
         img_path = images[i]
         mask_path = masks[i]
-        # read image and convert it to RGB scale
-        image = cv2.imread(img_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # read mask
-        mask = cv2.imread(mask_path)
-        # sho image and mask
-        plt.imshow(image)
-        plt.imshow(mask, alpha=0.4)
 
+        image = np.load(img_path)
+        mask = np.load(mask_path)
+
+        plt.imshow(image.squeeze(), cmap='gray')
+        plt.imshow(mask.squeeze(), cmap='jet', alpha=0.4)
         plt.axis('off')
 
     plt.tight_layout()
@@ -83,20 +83,16 @@ def init_data(data_dir):
     df = create_df(data_dir)
     train_df, valid_df, test_df = split_df(df)
 
-    # print(train_df)
-    # print(valid_df)
-    # print(test_df)
-
-
     tr_aug_dict = dict(rotation_range=0.2,
-                                width_shift_range=0.05,
-                                height_shift_range=0.05,
-                                shear_range=0.05,
-                                zoom_range=0.05,
-                                horizontal_flip=True,
-                                fill_mode='nearest')
+                       width_shift_range=0.05,
+                       height_shift_range=0.05,
+                       shear_range=0.05,
+                       zoom_range=0.05,
+                       horizontal_flip=True,
+                       fill_mode='nearest')
 
     train_gen = create_gens(train_df, aug_dict=tr_aug_dict)
     valid_gen = create_gens(valid_df, aug_dict={})
     test_gen = create_gens(test_df, aug_dict={})
+
     return train_gen, valid_gen, test_gen, train_df, valid_df, test_df
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from data_gen import *
 from matplotlib.pyplot import *
 from keras.optimizers import Adam, Adamax
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+import tensorflow as tf
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
@@ -13,27 +14,27 @@ if gpus:
     except RuntimeError as e:
         print(e)
 
-
 model = unet()
-# model = load_model('unet.hdf5')
-
-model.compile(Adamax(learning_rate= 0.001), loss= 'binary_crossentropy', metrics= ['accuracy', iou_coef, dice_coef])
-model.summary()
+model.compile(Adamax(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy', iou_coef, dice_coef])
 
 data_dir = 'Train/'
 epochs = 10
 batch_size = 16
-callbacks = [ModelCheckpoint('unet.hdf5', verbose=0, save_best_only=True)]
+callbacks = [ModelCheckpoint('unet.hdf5', verbose=1, save_best_only=True)]
 
 train_gen, valid_gen, test_gen, train_df, valid_df, test_df = init_data(data_dir)
 
+steps_per_epoch = len(train_df) // batch_size
+validation_steps = len(valid_df) // batch_size
+
 history = model.fit(train_gen,
-                    steps_per_epoch=len(train_df) / batch_size,
+                    steps_per_epoch=steps_per_epoch,
                     epochs=epochs,
                     verbose=1,
                     callbacks=callbacks,
-                    validation_data = valid_gen,
-                    validation_steps=len(valid_df) / batch_size)
+                    validation_data=valid_gen,
+                    validation_steps=validation_steps)
 
 model.save("model_114514.h5")
+
 
